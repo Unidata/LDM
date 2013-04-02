461160929c62c84f786dffd0700f1f79e9cf7877
Steven Emmerson <steve@unidata.ucar.edu>
2013-04-02 11:34:31 -0600

    Updated copyright notice.

--- noaaport/ldmProductQueue.c
+++ noaaport/ldmProductQueue.c
@@ -1,5 +1,6 @@
 /*
- *   Copyright 2011, University Corporation for Atmospheric Research.
+ *   Copyright 2013, University Corporation for Atmospheric Research.
+ *   All rights reserved.
  *   See file COPYRIGHT in the top-level source-directory for copying and
  *   redistribution conditions.
  */
