e9fbe1b18b134ea5eabef665dbeae0bf67b36339
steve <steve@unidata.ucar.edu>
2014-02-04 10:06:35 -0700

    Removed "--disable-root-actions" in CI-commit.sh

--- CI-commit.sh
+++ CI-commit.sh
@@ -12,9 +12,8 @@ mkdir -p m4
 autoreconf -i --force
 ./configure --disable-root-actions --with-noaaport --with-retrans \
         --with-gribinsert --with-multicast &>configure.log
-make distcheck
-#make distcheck DISTCHECK_CONFIGURE_FLAGS='--disable-root-actions'
-#make distcheck DISTCHECK_CONFIGURE_FLAGS='--disable-root-actions --with-multicast'
-#make distcheck DISTCHECK_CONFIGURE_FLAGS='--disable-root-actions --with-gribinsert'
-#make distcheck DISTCHECK_CONFIGURE_FLAGS='--disable-root-actions --with-noaaport'
-#make distcheck DISTCHECK_CONFIGURE_FLAGS='--disable-root-actions --with-noaaport --with-retrans'
+make distcheck DISTCHECK_CONFIGURE_FLAGS=''
+#make distcheck DISTCHECK_CONFIGURE_FLAGS='--with-multicast'
+#make distcheck DISTCHECK_CONFIGURE_FLAGS='--with-gribinsert'
+#make distcheck DISTCHECK_CONFIGURE_FLAGS='--with-noaaport'
+#make distcheck DISTCHECK_CONFIGURE_FLAGS='--with-noaaport --with-retrans'
