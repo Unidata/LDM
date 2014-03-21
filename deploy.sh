b3fe9fd4bd0deb2c1ae06c18c073c97bd9f0a8e3
steve <steve@unidata.ucar.edu>
2014-03-20 19:47:47 -0600

    Remove "-x" option from exec of /bin/sh

--- deploy.sh
+++ deploy.sh
@@ -25,7 +25,7 @@ trap "ssh ldm@$host rm -f $srcDistroName; `trap -p ERR`" ERR
 # As the LDM user on the remote host, unpack, build, and install the package.
 #
 ssh -T ldm@$host <<EOF
-exec /bin/sh -ex
+exec /bin/sh -e
 gunzip -c $srcDistroName | pax -r '-s:/:/src/:'
 trap "rm -rf \$HOME/$pkgName; \`trap -p ERR\`" ERR
 rm $srcDistroName
@@ -37,7 +37,7 @@ EOF
 # As the superuser on the remote host, perform the root actions.
 #
 ssh -T root@$host <<EOF
-exec /bin/sh -ex
+exec /bin/sh -e
 ldmHome=`awk -F: '$1~/^ldm$/{print $6}' /etc/passwd`
 cd \$ldmHome/$pkgName/src
 make root-actions >root-actions.log 2>&1
@@ -46,7 +46,7 @@ EOF
 # As the LDM user on the remote host, execute the new package.
 #
 ssh -T ldm@$host <<EOF
-exec /bin/sh -ex
+exec /bin/sh -e
 ldmadmin isrunning && ldmadmin stop
 rm -f runtime
 ln -s $pkgName runtime
