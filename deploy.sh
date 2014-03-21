8a546d15cc34b09a9174ef93754fc4665941f2af
steve <steve@unidata.ucar.edu>
2014-03-20 18:38:54 -0600

    Escaped $ in remotely-executed here-document

--- deploy.sh
+++ deploy.sh
@@ -25,7 +25,7 @@ trap "ssh ldm@$host rm -f $srcDistroName; `trap -p ERR`" ERR
 # As the LDM user on the remote host, unpack, build, and install the package.
 #
 ssh -T ldm@$host <<EOF
-exec /bin/sh -e
+exec /bin/sh -ex
 gunzip -c $srcDistroName | pax -r '-s:/:/src/:'
 trap "rm -rf \$HOME/$pkgName; \`trap -p ERR\`" ERR
 rm $srcDistroName
@@ -37,16 +37,16 @@ EOF
 # As the superuser on the remote host, perform the root actions.
 #
 ssh -T root@$host <<EOF
-exec /bin/sh -e
+exec /bin/sh -ex
 ldmHome=`awk -F: '$1~/^ldm$/{print $6}' /etc/passwd`
-cd $ldmHome/$pkgName/src
+cd \$ldmHome/$pkgName/src
 make root-actions >root-actions.log 2>&1
 EOF
 
 # As the LDM user on the remote host, execute the new package.
 #
 ssh -T ldm@$host <<EOF
-exec /bin/sh -e
+exec /bin/sh -ex
 ldmadmin isrunning && ldmadmin stop
 rm -f runtime
 ln -s $pkgName runtime
