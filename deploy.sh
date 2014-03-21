9d2c1f2ddbe2c4e8b65c105d18edf163eea415ee
steve <steve@unidata.ucar.edu>
2014-03-21 10:15:32 -0600

    Add "--login" to bash(1) invocations to obtain correct PATH

--- deploy.sh
+++ deploy.sh
@@ -20,11 +20,11 @@ pkgName=`basename $srcDistroName .tar.gz`
 # Copy the source-distribution to the remote host.
 #
 scp $srcDistroPath ldm@$host:
-trap "ssh ldm@$host rm -f $srcDistroName; `trap -p ERR`" ERR
+trap "ssh -T ldm@$host rm -f $srcDistroName; `trap -p ERR`" ERR
 
 # As the LDM user on the remote host, unpack, build, and install the package.
 #
-ssh -T ldm@$host bash -x -e <<EOF
+ssh -T ldm@$host bash --login -x -e <<EOF
 gunzip -c $srcDistroName | pax -r '-s:/:/src/:'
 trap "rm -rf \$HOME/$pkgName; \`trap -p ERR\`" ERR
 rm $srcDistroName
@@ -35,7 +35,7 @@ EOF
 
 # As the superuser on the remote host, perform the root actions.
 #
-ssh -T root@$host bash -x -e <<EOF
+ssh -T root@$host bash --login -x -e <<EOF
 ldmHome=\`awk -F: '\$1~/^ldm$/{print \$6}' /etc/passwd\`
 cd \$ldmHome/$pkgName/src
 make root-actions >root-actions.log 2>&1
@@ -43,7 +43,7 @@ EOF
 
 # As the LDM user on the remote host, execute the new package.
 #
-ssh -T ldm@$host bash -x -e <<EOF
+ssh -T ldm@$host bash --login -x -e <<EOF
 ldmadmin isrunning && ldmadmin stop
 rm -f runtime
 ln -s $pkgName runtime
