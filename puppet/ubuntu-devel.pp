7bfe7965b4c6f7c90afc760ebda933ef9fa665d2
steve <steve@unidata.ucar.edu>
2014-03-21 16:02:03 -0600

    Add "apt-get update" and dependencies to install XML2.
    The header-files get removed by a "vagrant destroy".

--- puppet/ubuntu-devel.pp
+++ puppet/ubuntu-devel.pp
@@ -5,12 +5,21 @@ group { "puppet":
 File { owner => 0, group => 0, mode => 0644 }
 Exec { path => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'], }
 
+exec {'update': command => 'apt-get -y update', }
+
 package {'make':
   ensure  => present,
+  require => Exec['update'],
+}
+
+package {'libxml2-dev':
+  ensure  => present,
+  require => Exec['update'],
 }
 
 package {'libpng-dev':
   ensure  => present,
+  require => Exec['update'],
 }
 
 exec {'bashrc':
