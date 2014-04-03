cb744cb2279ad057cb19646002f175b326c5c0ee
steve <steve@unidata.ucar.edu>
2014-04-03 12:49:52 -0600

    Improve publishing and correct install-data macro

--- publish.sh
+++ publish.sh
@@ -26,7 +26,7 @@ pax -zr <$SOURCE_DISTRO_NAME
 trap "rm -rf $PKG_ID; `trap -p EXIT`" EXIT
 cd $PKG_ID
 ./configure --prefix=$ABSPATH_DEFAULT_INSTALL_PREFIX --disable-root-actions \
-        >configure.log 2>&1
+        --with-noaaport >configure.log 2>&1
 make install DESTDIR=$PKG_ID >install.log 2>&1
 
 # Copy the documentation to the package's website.
@@ -42,9 +42,6 @@ ssh -T $WEB_HOST mv -f $versionWebDirTmp $ABSPATH_VERSION_WEB_DIR
 # documentation.
 ssh -T $WEB_HOST bash --login <<EOF
     set -ex  # Exit on error
-
-    version=`echo $PKG_ID | sed 's/.*-//'`
-    indexHtml=index.html
 
     # Go to the top-level of the package's web-pages.
     #
@@ -64,7 +61,7 @@ ssh -T $WEB_HOST bash --login <<EOF
     sed -n '1,/$BEGIN_VERSION_LINKS/p' index.html >index.html.new
     for vers in \`cat versions\`; do
         href=\`find $PKG_NAME-\$vers -name index.html\`
-        test "\$href" || href=\`find $PKG_NAME-\$vers -name \$indexHtml\`
+        test "\$href"
         echo "            <li><a href=\"\$href\">\$vers</a>" >>index.html.new
     done
     sed -n '/$END_VERSION_LINKS/,\$p' index.html >>index.html.new
