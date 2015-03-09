d191a59065e6e08a59841bfa53c5a777ffd872ef
steve <steve@unidata.ucar.edu>
2015-03-09 14:01:25 -0600

    Improved internal commentary

--- delivery/CI-commit.sh
+++ delivery/CI-commit.sh
@@ -21,7 +21,8 @@ mkdir -p m4 mcast_lib/vcmtp/m4
 autoreconf -if
 ./configure --disable-root-actions >configure.log 2>&1
 make distcheck
-# DISTCHECK_CONFIGURE_FLAGS overrides AM_DISTCHECK_CONFIGURE_FLAGS.
+# DISTCHECK_CONFIGURE_FLAGS appends to AM_DISTCHECK_CONFIGURE_FLAGS, which
+# exists in automake(1) 1.13 but not in 1.11.
 # Keep LDMHOME value consonant with that in `Makefile.am`:
 COM_OPTS='--disable-root-actions 'LDMHOME=`cd ../_inst && pwd`
 make distcheck DISTCHECK_CONFIGURE_FLAGS='--with-multicast '$COM_OPTS
