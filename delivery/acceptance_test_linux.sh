507b4d0912398409585e058f7055769730b44adf
steve <steve@unidata.ucar.edu>
2015-03-09 14:01:52 -0600

    Reverted to flock(1) on just `vagrant up` but included `sleep 30`

--- delivery/acceptance_test_linux.sh
+++ delivery/acceptance_test_linux.sh
@@ -32,8 +32,8 @@ vmName=${2:?Name of Vagrant virtual-machine not specified}
 # by a one-line flock(1) command or by an flock(1) block. Consequently,
 # the concurrent Vagrant/VirtualBox VM-s are serialized.
 #
-( flock 9; vagrant up $vmName
-#) 9>/tmp/`basename $0`-$USER
+( flock 9; vagrant up $vmName; sleep 30
+) 9>/tmp/`basename $0`-$USER
 #flock /tmp/`basename $0`-$USER vagrant up $vmName
 trap "vagrant destroy --force $vmName; `trap -p EXIT`" EXIT
 
@@ -56,4 +56,4 @@ vagrant ssh $vmName -- -T <<EOF
     ./configure $ACCEPTANCE_CONFIGURE_OPTS
     make all check install
 EOF
-) 9>/tmp/`basename $0`-$USER
+#) 9>/tmp/`basename $0`-$USER
