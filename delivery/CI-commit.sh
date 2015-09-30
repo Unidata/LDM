fa31a5e46dd1d54d0c40d2d389d1c2e60f4125f9
steve <steve@unidata.ucar.edu>
2015-09-30 15:34:26 -0600

    Reduce "make distcheck" to just one.

--- delivery/CI-commit.sh
+++ delivery/CI-commit.sh
@@ -21,11 +21,12 @@ PATH=/usr/bin:$PATH
 mkdir -p m4 mcast_lib/vcmtp/m4
 autoreconf -if
 ./configure --disable-root-actions >configure.log 2>&1
+# Assumes AM_DISTCHECK_CONFIGURE_FLAGS contains all "--with-..." options
 make distcheck
 # DISTCHECK_CONFIGURE_FLAGS appends to AM_DISTCHECK_CONFIGURE_FLAGS, which
 # exists in automake(1) 1.13 but not in 1.11.
-COM_OPTS='--disable-root-actions'
-#make distcheck DISTCHECK_CONFIGURE_FLAGS='--with-multicast '$COM_OPTS
-make distcheck DISTCHECK_CONFIGURE_FLAGS='--with-gribinsert '$COM_OPTS
-make distcheck DISTCHECK_CONFIGURE_FLAGS='--with-noaaport '$COM_OPTS
-make distcheck DISTCHECK_CONFIGURE_FLAGS='--with-noaaport --with-retrans '$COM_OPTS
+#COM_OPTS='--disable-root-actions'
+##make distcheck DISTCHECK_CONFIGURE_FLAGS='--with-multicast '$COM_OPTS
+#make distcheck DISTCHECK_CONFIGURE_FLAGS='--with-gribinsert '$COM_OPTS
+#make distcheck DISTCHECK_CONFIGURE_FLAGS='--with-noaaport '$COM_OPTS
+#make distcheck DISTCHECK_CONFIGURE_FLAGS='--with-noaaport --with-retrans '$COM_OPTS
