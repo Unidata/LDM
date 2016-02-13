26c91acc1e33d9d598c1783fe72e9c3bfefa9ca0
steve <steve@unidata.ucar.edu>
2016-02-13 09:37:25 -0700

    Fixed potential NULL dereference.

--- noaaport/readnoaaport.c
+++ noaaport/readnoaaport.c
@@ -974,8 +974,8 @@ int main(
 
         if (GOES == 1) {
             if (pfrag->fragnum > 0) {
-                if ((pfrag->fragnum != prod.tail->fragnum + 1) || 
-                        (pfrag->seqno != prod.seqno)) {
+                if (prod.tail && ((pfrag->fragnum != prod.tail->fragnum + 1) ||
+                        (pfrag->seqno != prod.seqno))) {
                     log_error("Missing GOES fragment in sequence, "
                         "last %d/%d this %d/%d\0", prod.tail->fragnum,
                         prod.seqno, pfrag->fragnum, pfrag->seqno);
