86fa5a4ccdfcabc45855375f49877cc72bb85140
Steven Emmerson <emmerson@ucar.edu>
2017-12-14 13:43:45 -0700

    Eliminated "pImpl" use because template instantiation nullifies benefit

--- mcast_lib/C++/FixedDelayQueue.h
+++ mcast_lib/C++/FixedDelayQueue.h
@@ -21,151 +21,102 @@
 
 /**
  * @tparam Value     Type of value being stored in the queue. Must support
- *                   copy assignment and move assignment.
- * @tparam Dur       Duration type (e.g., std::chrono::seconds)
+ *                   default construction and copy assignment.
+ * @tparam Duration  Type of duration (e.g., std::chrono::seconds)
  */
-template<typename Value, typename Dur>
+template<typename Value, typename Duration>
 class FixedDelayQueue final
 {
 public:
-    typedef Dur                       Duration;
+    typedef Duration                  DurType;
+    typedef Value                     ValType;
 
 private:
     typedef std::chrono::steady_clock Clock;
     typedef Clock::time_point         TimePoint;
+
     /**
-     * Implementation of `FixedDelayQueue`.
+     * The "pImpl" idiom isn't used because that would prevent template class
+     * instantiation by simply including the header-file.
      */
-    class Impl final
-    {
-        /**
-         * An element in the queue.
-         */
-        class Element final {
-            Value     value; /// Value.
-            TimePoint when;  /// Reveal-time.
 
-        public:
-            /**
-             * Constructs from a value and a delay.
-             * @param[in] value  Value
-             * @param[in] delay  Delay for element until reveal-time
-             */
-            Element(const Value&    value,
-                    const Duration& delay)
-                : value{}
-                , when{Clock::now() + delay}
-            {
-                this->value = value;
-            }
-
-            /**
-             * Returns the value.
-             * @return           Value
-             * @exceptionsafety  Strong guarantee
-             * @threadsafety     Safe
-             */
-            inline Value getValue() const noexcept
-            {
-                return value;
-            }
-
-            /**
-             * Returns the reveal-time.
-             * @return           Reveal-time.
-             * @exceptionsafety  Strong guarantee
-             * @threadsafety     Safe
-             */
-            inline const TimePoint& getTime() const noexcept
-            {
-                return when;
-            }
-        };
-
-        /// The mutex for concurrent access to the queue.
-        std::mutex mutable      mutex;
-        /// The condition variable for signaling when the queue has been modified
-        std::condition_variable cond;
-        /// The queue.
-        std::queue<Element>     queue;
-        /// Minimum residence-time (i.e., delay-time) for an element in the queue
-        Duration                delay;
+    /**
+     * An element in the queue.
+     */
+    class Element final {
+        Value     value; /// Value.
+        TimePoint when;  /// Reveal-time.
 
     public:
         /**
-         * Constructs from a delay.
-         * @param[in] delay           Delay for each element in units of
-         *                            template parameter
-         * @throws std::bad_alloc     Necessary memory can't be allocated
-         * @throws std::system_error  System error
+         * Constructs from a value and a delay.
+         * @param[in] value  Value
+         * @param[in] delay  Delay for element until reveal-time
          */
-        explicit Impl(const Duration delay)
-            : mutex{}
-            , cond{}
-            , queue{}
-            , delay{delay}
-        {}
-
-        /**
-         * Adds a value to the queue.
-         * @param[in] value  Value to be added
-         * @exceptionsafety  Strong guarantee
-         * @threadsafety     Safe
-         */
-        void push(const Value& value)
+        Element(const Value&    value,
+                const Duration& delay)
+            : value{}
+            , when{Clock::now() + delay}
         {
-            std::unique_lock<std::mutex>(mutex);
-            queue.emplace(value, delay);
-            cond.notify_one();
+            this->value = value;
         }
 
         /**
-         * Returns the value whose reveal-time is the earliest and not later
-         * than the current time and removes it from the queue. Blocks until
-         * such a value is available.
-         * @return          Value with earliest reveal-time that's not later
-         *                  than current time
-         * @exceptionsafety Strong guarantee
-         * @threadsafety    Safe
+         * Returns the value.
+         * @return           Value
+         * @exceptionsafety  Strong guarantee
+         * @threadsafety     Safe
          */
-        Value pop()
+        inline Value getValue() const noexcept
         {
-            std::unique_lock<std::mutex> lock(mutex);
-            while (queue.size() == 0)
-                cond.wait(lock);
-            const auto time = queue.front().getTime();
-            while (time > Clock::now())
-                cond.wait_until(lock, time);
-            const auto value = queue.front().getValue();
-            queue.pop(); // Not empty => nothrow
             return value;
         }
 
         /**
-         * Returns the number of values in the queue.
-         * @return           Number of values in queue
-         * @exceptionsafety  Nothrow
+         * Returns the reveal-time.
+         * @return           Reveal-time.
+         * @exceptionsafety  Strong guarantee
          * @threadsafety     Safe
          */
-        size_t size() const noexcept
+        inline const TimePoint& getTime() const noexcept
         {
-            std::lock_guard<std::mutex> lock(mutex);
-            return queue.size();
+            return when;
         }
     };
-    std::shared_ptr<Impl> pImpl;
+
+    typedef std::mutex             Mutex;
+    typedef std::lock_guard<Mutex> LockGuard;
+
+    mutable Mutex           mutex;
+    std::condition_variable cond;
+    std::queue<Element>     queue;
+    /// Minimum residence-time (i.e., delay-time) for element in queue
+    Duration                delay;
 
 public:
     /**
      * Constructs from a delay.
-     * @param[in] delay           Delay for each element in units of template
-     *                            parameter
+     * @param[in] delay           Delay for each element in units of
+     *                            template parameter
      * @throws std::bad_alloc     Necessary memory can't be allocated
      * @throws std::system_error  System error
      */
-    explicit FixedDelayQueue(Dur delay)
-        : pImpl{new Impl{delay}}
+    explicit FixedDelayQueue(const Duration delay)
+        : mutex{}
+        , cond{}
+        , queue{}
+        , delay{delay}
     {}
+
+    /**
+     * Because the "pImpl" idiom isn't being used, copy and move construction
+     * and assignment are disallowed to obviate having to think about them. This
+     * decision can be revisited if necessary.
+     */
+    FixedDelayQueue(const FixedDelayQueue& that) =delete;
+    FixedDelayQueue(FixedDelayQueue&& that) =delete;
+    FixedDelayQueue& operator=(const FixedDelayQueue& rhs) =delete;
+    FixedDelayQueue& operator=(const FixedDelayQueue&& rhs) =delete;
 
     /**
      * Adds a value to the queue.
@@ -175,21 +126,31 @@ public:
      */
     void push(const Value& value)
     {
-        pImpl->push(value);
+        LockGuard lock{mutex};
+        queue.emplace(value, delay);
+        cond.notify_one();
     }
 
     /**
-     * Returns the value whose reveal-time is the earliest and not later than
-     * the current time and removes it from the queue. Blocks until such a value
-     * is available.
-     * @return           Value with earliest reveal-time that's not later than
-     *                   current time
-     * @exceptionsafety  Basic guarantee
-     * @threadsafety     Safe
+     * Returns the value whose reveal-time is the earliest and not later
+     * than the current time and removes it from the queue. Blocks until
+     * such a value is available.
+     * @return          Value with earliest reveal-time that's not later
+     *                  than current time
+     * @exceptionsafety Strong guarantee
+     * @threadsafety    Safe
      */
     Value pop()
     {
-        return pImpl->pop();
+        std::unique_lock<Mutex> lock(mutex);
+        while (queue.size() == 0)
+            cond.wait(lock);
+        const auto time = queue.front().getTime();
+        while (time > Clock::now())
+            cond.wait_until(lock, time);
+        const auto value = queue.front().getValue();
+        queue.pop(); // Not empty => nothrow
+        return value;
     }
 
     /**
@@ -200,7 +161,8 @@ public:
      */
     size_t size() const noexcept
     {
-        return pImpl->size();
+        LockGuard lock(mutex);
+        return queue.size();
     }
 };
 
