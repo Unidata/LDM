2cfcd82c0646ae5a9cc2e3801f6c44f8b156b9ae
Yuanlong Tan <30730116+yt4xb@users.noreply.github.com>
2018-03-01 10:59:25 -0500

    Update remove.py

--- OESS-Client-V2/remove.py
+++ OESS-Client-V2/remove.py
@@ -5,32 +5,48 @@ import time
 import string
 import array
 import numpy
-global values
-values = {}
-file_name = str('Prov_serv_' + sys.argv[1] + '.log')
-gh_url = 'https://al2s.net.internet2.edu/oess/services-kerb/provisioning.cgi'
-if (sys.argv[1] == 'fail_over_circuit'):
-        values = {'action' : 'fail_over_circuit', 'workgroup_id' : sys.argv[2], 'circuit_id' : sys.argv[3]}
-elif (sys.argv[1] == 'reprovision_circuit'):
-        values = {'action' : 'reprovision_circuit', 'workgroup_id' : sys.argv[2], 'circuit_id' : sys.argv[3]}
-elif (sys.argv[1] == 'remove_circuit'):
-        values = {'action' : 'remove_circuit', 'workgroup_id' : sys.argv[2], 'circuit_id' : sys.argv[3], 'remove_time' : -1}
-
-data = urllib.urlencode(values)
-req = urllib2.Request(gh_url, data)
-#print data
+import json
+global values1, values2
+values1 = {}
+values2 = {}
+link = []
+gh_url = 'https://al2s.net.internet2.edu/oess/services-kerb/data.cgi'
+values1 = {'method' : 'get_workgroups'}
+data1 = urllib.urlencode(values1, doseq=True)
+req1 = urllib2.Request(gh_url, data1)
 password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
 password_manager.add_password(None, gh_url, 'uva4api', 'AL2Ssenyd2011!')
-
 auth_manager = urllib2.HTTPBasicAuthHandler(password_manager)
 opener = urllib2.build_opener(auth_manager)
-
 urllib2.install_opener(opener)
+handler1 = urllib2.urlopen(req1)
+result1 = handler1.read()
+jsonData1 = json.loads(result1)
 
+searchResults1 =  jsonData1['results']
+#print searchResults1
+for er in searchResults1:
+	if er['name'] == sys.argv[1]:
+		wg_id = er['workgroup_id']
+#print wg_id
+
+values2 = values = {'action' : 'remove_circuit', 'workgroup_id' : wg_id, 'circuit_id' : sys.argv[2], 'remove_time' : -1}
+data = urllib.urlencode(values2, doseq=True)
+#print data
+gh_url2 = 'https://al2s.net.internet2.edu/oess/services-kerb/provisioning.cgi'
+req = urllib2.Request(gh_url2, data)
+
+password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
+password_manager.add_password(None, gh_url2, 'uva4api', 'AL2Ssenyd2011!')
+auth_manager = urllib2.HTTPBasicAuthHandler(password_manager)
+opener = urllib2.build_opener(auth_manager)
+urllib2.install_opener(opener)
 handler = urllib2.urlopen(req)
-f = open (file_name, 'w')
-#for line in handler.read()
-print handler.readline()
-f.write(handler.read())
-print handler.getcode()
-#print handler.headers.getheader('content-type')
+result = handler.read()
+jsonData = json.loads(result)
+searchResults = jsonData['results']
+
+if (searchResults == None):
+    print 1
+else:
+    print 0
