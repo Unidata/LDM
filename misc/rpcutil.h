038cbd6bcc58d83a66cec65f90c7e56014a2232b
Steven Emmerson <emmerson@ucar.edu>
2018-03-15 13:00:37 -0600

    Include <stddef.h> and add #if __cplusplus

--- misc/rpcutil.h
+++ misc/rpcutil.h
@@ -2,6 +2,11 @@
 #define RPCUTIL_H
 
 #include <rpc/rpc.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
 
 char *clnt_errmsg(CLIENT* clnt);
 int local_portmapper_running();
@@ -14,5 +19,9 @@ int local_portmapper_running();
 const char*
 rpc_getClientId(
     struct svc_req* const rqstp);
+
+#ifdef __cplusplus
+    }
+#endif
 
 #endif
