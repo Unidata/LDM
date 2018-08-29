0dfe444223da37b0d6656bf031f115a5f9923c39
Steven Emmerson <emmerson@ucar.edu>
2018-08-29 15:07:01 -0600

    Squashed 'OESS/' content from commit a990c61
    git-subtree-dir: OESS
    git-subtree-split: a990c61e5c5b322647026494a2989ce9f7307761

---
+++ OESS-Client-Version_1/netInfo.py
@@ -0,0 +1,68 @@
+import urllib2
+import urllib
+import sys
+import time
+import string
+global values
+values = {}
+file_name = str('NetInfo_' + sys.argv[1] + '.log')
+gh_url = 'https://al2s.net.internet2.edu/oess/services-kerb/data.cgi'
+result = 'get_' + sys.argv[1]
+if (result == 'get_workgroups'):
+	values = {'action' : result}
+elif (result == 'get_nodes'): 
+	values = {'action' : result}	
+elif (result == 'get_node_interfaces'):
+        values = {'action' : result, 'node' : sys.argv[2], 'show_trunk' : 1}
+elif (result == 'get_all_node_status'):
+        values = {'action' : result}
+elif (result == 'get_all_link_status'):
+        values = {'action' : result}
+elif (result == 'get_all_resources_for_workgroup'):
+        values = {'action' : result, 'workgroup_id' : sys.argv[2]}
+elif (result == 'get_maps'):
+        values = {'action' : result, 'workgroup_id' : sys.argv[2], 'link_type' : 'openflow'}
+elif (result == 'get_interface'):
+	values = {'action' : result, 'interface_id' : sys.argv[2]}
+elif (result == 'get_workgroup_interfaces'):
+        values = {'action' : result, 'workgroup_id' : sys.argv[2]}    
+elif (result == 'get_existing_circuits'):
+        values = {'action' : result, 'workgroup_id' : sys.argv[2]}  
+elif (result == 'get_circuits_by_interface_id'):
+	values = {'action' : result, 'interface_id' : sys.argv[2]}
+elif (result == 'get_circuit_details'):
+        values = {'action' : result, 'circuit_id' : sys.argv[2]}
+elif (result == 'get_circuit_scheduled_events'):         
+        values = {'action' : result, 'circuit_id' : sys.argv[2]}
+elif (result == 'get_circuit_history'):         
+        values = {'action' : result, 'circuit_id' : sys.argv[2]}
+elif (result == 'get_workgroup_members'):         
+        values = {'action' : result, 'workgroup_id' : sys.argv[2]}
+elif (result == 'get_vlan_tag_range'):
+        values = {'action' : result, 'workgroup_id' : sys.argv[2], 'node' : sys.argv[3], 'interface' : sys.argv[4]}
+elif (sys.argv[1] == 'generate_clr'):
+	values = {'action' : 'generate_clr', 'circuit_id' : sys.argv[2]}
+elif (sys.argv[1] == 'is_vlan_tag_available'):
+	values = {'action' : 'is_vlan_tag_available', 'node' : sys.argv[2], 'interface' : sys.argv[3], 'vlan' : sys.argv[4]}
+else:
+	print "Input Method Error!"
+data = urllib.urlencode(values)
+req = urllib2.Request(gh_url, data)
+
+password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
+password_manager.add_password(None, gh_url, 'name', 'passwd')
+auth_manager = urllib2.HTTPBasicAuthHandler(password_manager)
+opener = urllib2.build_opener(auth_manager)
+
+urllib2.install_opener(opener)
+
+handler = urllib2.urlopen(req)
+f = open (file_name, 'w')
+#for line in handler.read()
+print handler.readline()
+f.write(handler.read())
+print handler.getcode()
+print handler.headers.getheader('content-type')
+
+
+
---
+++ OESS-Client-Version_1/provision.py
@@ -0,0 +1,33 @@
+import urllib2
+import urllib
+import sys
+import time
+import string
+import array
+import numpy
+global values
+file_name = str(sys.argv[2] + '.log')
+gh_url = 'https://al2s.net.internet2.edu/oess/services-kerb/provisioning.cgi'
+values = {}
+#workgroup_id=611, link=I2-ASHB-PITT-100GE-07737,
+#node=sdn-sw.ashb.net.internet2.edu, interface=et-3/0/0, tag=332
+#node=sdn-sw.pitt.net.internet2.edu, interface=et-8/0/0, tag=332
+#provision_time=-1 means immediately, remove_time = -1 means never.
+values = {'method' : 'provision_circuit', 'workgroup_id' : sys.argv[1], 'provision_time' : -1, 'remove_time' : -1, 'description' : sys.argv[2], 'link' : sys.argv[3], 'node' : [sys.argv[4], sys.argv[7]], 'interface' : [sys.argv[5], sys.argv[8]], 'tag' : [sys.argv[6], sys.argv[9]]}
+data = urllib.urlencode(values, doseq=True)
+req = urllib2.Request(gh_url, data)
+#print data
+password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
+password_manager.add_password(None, gh_url, 'name', 'passwd')
+
+auth_manager = urllib2.HTTPBasicAuthHandler(password_manager)
+opener = urllib2.build_opener(auth_manager)
+
+urllib2.install_opener(opener)
+
+handler = urllib2.urlopen(req)
+f = open (file_name, 'w')
+print handler.readline()
+f.write(handler.read())
+print handler.getcode()
+print handler.headers.getheader('content-type')
