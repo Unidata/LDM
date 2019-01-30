f4a851e81419c2a5cbdab5eca13646fb77b394e0
Steven Emmerson <emmerson@ucar.edu>
2019-01-30 14:12:10 -0700

    Make publish(1) stand-alone. Might not work.

--- delivery/publish.sh
+++ delivery/publish.sh
@@ -5,6 +5,10 @@
 #
 # Usage:
 #       $0 <host> <version>
+# where:
+#       <host>     Name of the host on which the LDM package is made public
+#                  (e.g., "www.unidata.ucar.edu")
+#       <version>  LDM package version (e.g. "6.13.7")
 
 set -e  # exit on failure
 
@@ -12,8 +16,10 @@ test $# -eq 2
 
 host=${1?Host not specified}
 version=${2?Version not specified}
+
 tarball=ldm-$version.tar.gz
 ftpdir=/web/ftp/pub/software/ldm
+webdir=/web/content/software/ldm
 
 # Ensure that the FTP directory contains the tarball
 if ! ssh $host test -e $ftpdir/$tarball; then
@@ -43,32 +49,28 @@ EOF
 make install >&install.log && echo Installed
 
 # Copy the documentation to the package's website
-versionWebDir=/web/content/software/ldm/ldm-$version
+versionWebDir=$webdir/ldm-$version
 ssh -T $host rm -rf $versionWebDir
 trap "ssh -T $host rm -rf $versionWebDir; `trap -p ERR`" ERR
 scp -Br ../share/doc/ldm $host:$versionWebDir
 
 # Ensure that the package's home-page references the just-copied documentation.
-ssh -T steve@$WEB_HOST bash --login <<EOF
+ssh -T $host bash --login <<EOF
     set -ex  # Exit on error
 
     # Go to the top-level of the package's web-pages.
-    #
-    cd /web/content/software/ldm
+    cd $webdir
 
     # Allow group write access to all created files.
-    #
     umask 02
 
     # Set the hyperlink references to the documentation. For a given major
     # and minor version, keep only the latest bug-fix.
-    #
     ls -d ldm-[0-9.]*.tar.gz |
         sed "s/ldm-//" |
         sort -t. -k 1nr,1 -k 2nr,2 -k 3nr,3 |
         awk -F. '\$1!=ma||\$2!=mi{print}{ma=\$1;mi=\$2}' >versions
-    sed -n '1,/$BEGIN_VERSION_LINKS/p' versions.inc >versions.inc.new
-    for vers in \`cat versions\`; do
+    sed -n '1,/BEGIN VERSION LINKS/p' versions.inc >versions.inc.new for vers in \`cat versions\`; do
         href=ldm-\$vers
         cat <<END_VERS >>versions.inc.new
              <tr>
@@ -88,19 +90,17 @@ ssh -T steve@$WEB_HOST bash --login <<EOF
 
 END_VERS
     done
-    sed -n '/$END_VERSION_LINKS/,\$p' versions.inc >>versions.inc.new
+    sed -n '/END VERSION LINKS/,\$p' versions.inc >>versions.inc.new
     rm -f versions.inc.old
     cp versions.inc versions.inc.old
     mv versions.inc.new versions.inc
 
     # Delete all versions not referenced in the top-level HTML file.
-    #
     for vers in \`ls -d ldm-[0-9.]*.tar.gz | sed "s/ldm-//"\`; do
         fgrep -s \$vers versions || rm -rf ldm-\$vers
     done
 
     # Set the symbolic link to the current version
-    #
     rm -f ldm-current
     ln -s ldm-$version ldm-current
 EOF
