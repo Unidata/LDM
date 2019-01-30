e9615bd65b4600062ce90c5be536df8a11a34545
Steven Emmerson <emmerson@ucar.edu>
2019-01-30 13:05:17 -0700

    Working on new release mechanism

--- delivery/publish.sh
+++ delivery/publish.sh
@@ -1,75 +1,60 @@
 # Copies an LDM source distribution to the public download directory and ensures
 # the existence of that version's documentation on the package's website. The
-# package is built and installed in "/tmp/$PKG_ID" to extract the documentation.
+# package is built and installed in "/tmp/ldm-$version" to extract the
+# documentation.
 #
 # Usage:
-#       $0
+#       $0 <host> <version>
 
 set -e  # exit on failure
 
-test $# -eq 0
+test $# -eq 2
 
-# If the source repository doesn't have the source distribution,
-#
-if ! ssh steve@$SOURCE_REPO_HOST test -e $ABSPATH_SOURCE_DISTRO; then
+host=${1?Host not specified}
+version=${2?Version not specified}
+tarball=ldm-$version.tar.gz
+ftpdir=/web/ftp/pub/software/ldm
+
+# Ensure that the FTP directory contains the tarball
+if ! ssh $host test -e $ftpdir/$tarball; then
     #
-    # Copy the source distribution to the source repository.
+    # Copy the tarball to the FTP directory.
     #
-    trap "ssh steve@$SOURCE_REPO_HOST rm -f $ABSPATH_SOURCE_DISTRO; `trap -p ERR`" ERR
-    scp $SOURCE_DISTRO_NAME steve@$SOURCE_REPO_HOST:$ABSPATH_SOURCE_DISTRO
+    trap "ssh $host rm -f $ftpdir/$tarball; `trap -p ERR`" ERR
+    scp $tarball $host:$tarball
 fi
 
-# Purge the source-repository of bug-fix versions that are older than the latest
+# Purge the FTP directory of bug-fix versions that are older than the latest
 # corresponding minor release.
-#
-ssh -T steve@$SOURCE_REPO_HOST bash --login <<EOF
+ssh -T $host bash --login <<EOF
     set -ex # Exit on error
-    cd $ABSPATH_SOURCE_REPO_DIR        
-    ls -d $PKG_ID_GLOB |
-        sed "s/$PKG_NAME-//" |
+    cd $ftpdir
+    ls -d ldm-[0-9.]*.tar.gz |
+        sed "s/ldm-//" |
         sort -t. -k 1nr,1 -k 2nr,2 -k 3nr,3 |
         awk -F. '\$1!=ma||\$2!=mi{print}{ma=\$1;mi=\$2}' >versions
-    for vers in \`ls -d $PKG_ID_GLOB | sed "s/$PKG_NAME-//"\`; do
-        fgrep -s \$vers versions || rm -rf $PKG_NAME-\$vers
+    for vers in \`ls -d ldm-[0-9.]*.tar.gz | sed "s/ldm-//"\`; do
+        fgrep -s \$vers versions || rm -rf ldm-\$vers
     done
 EOF
 
-# Install the package so that the documentation can be copied to the website.
-# "/tmp/$PKG_ID" is used as the installation point rather than anything under
-# the current directory because the current directory might have spaces in its
-# name and that causes problems for libtool(1).
-#
-rm -rf $PKG_ID
-pax -zr <$SOURCE_DISTRO_NAME
-trap "rm -rf $PKG_ID; `trap -p EXIT`" EXIT
-cd $PKG_ID
-./configure --prefix=$ABSPATH_DEFAULT_INSTALL_PREFIX --disable-root-actions \
-        --with-noaaport LDMHOME=$ABSPATH_DEFAULT_LDMHOME >configure.log 2>&1
-DESTDIR=/tmp/$PKG_ID
-rm -rf $DESTDIR
-make install DESTDIR=$DESTDIR >install.log 2>&1
+# Ensure that the package is installed so that the documentation can be copied
+# to the website.
+make install >&install.log && echo Installed
 
-# Copy the documentation to the package's website and delete the installation.
-#
-versionWebDirTmp=$ABSPATH_VERSION_WEB_DIR.tmp
-ssh -T steve@$WEB_HOST rm -rf $versionWebDirTmp
-trap "ssh -T steve@$WEB_HOST rm -rf $versionWebDirTmp; `trap -p ERR`" ERR
-scp -Br $DESTDIR$ABSPATH_DEFAULT_INSTALL_PREFIX/$RELPATH_DOC_DIR \
-        steve@$WEB_HOST:$versionWebDirTmp
-rm -r $DESTDIR
+# Copy the documentation to the package's website
+versionWebDir=/web/content/software/ldm/ldm-$version
+ssh -T $host rm -rf $versionWebDir
+trap "ssh -T $host rm -rf $versionWebDir; `trap -p ERR`" ERR
+scp -Br ../share/doc/ldm $host:$versionWebDir
 
 # Ensure that the package's home-page references the just-copied documentation.
 ssh -T steve@$WEB_HOST bash --login <<EOF
     set -ex  # Exit on error
 
-    # Install the just-copied documentation.
-    #
-    rm -rf $ABSPATH_VERSION_WEB_DIR
-    mv -f $versionWebDirTmp $ABSPATH_VERSION_WEB_DIR
-
     # Go to the top-level of the package's web-pages.
     #
-    cd $ABSPATH_PKG_WEB_DIR
+    cd /web/content/software/ldm
 
     # Allow group write access to all created files.
     #
@@ -78,13 +63,13 @@ ssh -T steve@$WEB_HOST bash --login <<EOF
     # Set the hyperlink references to the documentation. For a given major
     # and minor version, keep only the latest bug-fix.
     #
-    ls -d $PKG_ID_GLOB |
-        sed "s/$PKG_NAME-//" |
+    ls -d ldm-[0-9.]*.tar.gz |
+        sed "s/ldm-//" |
         sort -t. -k 1nr,1 -k 2nr,2 -k 3nr,3 |
         awk -F. '\$1!=ma||\$2!=mi{print}{ma=\$1;mi=\$2}' >versions
     sed -n '1,/$BEGIN_VERSION_LINKS/p' versions.inc >versions.inc.new
     for vers in \`cat versions\`; do
-        href=$PKG_NAME-\$vers
+        href=ldm-\$vers
         cat <<END_VERS >>versions.inc.new
              <tr>
               <td>
@@ -110,12 +95,12 @@ END_VERS
 
     # Delete all versions not referenced in the top-level HTML file.
     #
-    for vers in \`ls -d $PKG_ID_GLOB | sed "s/$PKG_NAME-//"\`; do
-        fgrep -s \$vers versions || rm -rf $PKG_NAME-\$vers
+    for vers in \`ls -d ldm-[0-9.]*.tar.gz | sed "s/ldm-//"\`; do
+        fgrep -s \$vers versions || rm -rf ldm-\$vers
     done
 
     # Set the symbolic link to the current version
     #
-    rm -f $PKG_NAME-current
-    ln -s $PKG_ID $PKG_NAME-current
+    rm -f ldm-current
+    ln -s ldm-$version ldm-current
 EOF
