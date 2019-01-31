383cc965d07fb5626a4c9babb13d98210049d022
Steven Emmerson <emmerson@ucar.edu>
2019-01-31 14:27:35 -0700

    Add messages describing what's happening

--- delivery/publish.sh
+++ delivery/publish.sh
@@ -26,12 +26,14 @@ if ! ssh $host test -e $ftpdir/$tarball; then
     #
     # Copy the tarball to the FTP directory.
     #
+    echo Copying  $tarbal to $ftpdir
     trap "ssh $host rm -f $ftpdir/$tarball; `trap -p ERR`" ERR
     scp $tarball $host:$tarball
 fi
 
 # Purge the FTP directory of bug-fix versions that are older than the latest
 # corresponding minor release.
+echo Purging $ftpdir of bug-fixes older than $tarball
 ssh -T $host bash --login <<EOF
     set -ex # Exit on error
     cd $ftpdir
@@ -46,10 +48,12 @@ EOF
 
 # Ensure that the package is installed so that the documentation can be copied
 # to the website.
-make install >&install.log && echo Installed
+echo Installing package locally
+make install >&install.log
 
 # Copy the documentation to the package's website
 versionWebDir=$webdir/ldm-$version
+echo Copying documentation to $versionWebDir
 ssh -T $host rm -rf $versionWebDir
 trap "ssh -T $host rm -rf $versionWebDir; `trap -p ERR`" ERR
 scp -Br ../share/doc/ldm $host:$versionWebDir
@@ -66,6 +70,7 @@ ssh -T $host bash --login <<EOF
 
     # Set the hyperlink references to the documentation. For a given major
     # and minor version, keep only the latest bug-fix.
+    echo Linking to documentation in $host:`pwd`
     ls -d ldm-[0-9.]*.tar.gz |
         sed "s/ldm-//" |
         sort -t. -k 1nr,1 -k 2nr,2 -k 3nr,3 |
@@ -96,11 +101,13 @@ END_VERS
     mv versions.inc.new versions.inc
 
     # Delete all versions not referenced in the top-level HTML file.
+    echo Deleting unreferenced version in $host:`pwd`
     for vers in \`ls -d ldm-[0-9.]*.tar.gz | sed "s/ldm-//"\`; do
         fgrep -s \$vers versions || rm -rf ldm-\$vers
     done
 
     # Set the symbolic link to the current version
+    echo Making ldm-$version the current version
     rm -f ldm-current
     ln -s ldm-$version ldm-current
 EOF
