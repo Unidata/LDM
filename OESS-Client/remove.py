2a2caf4616885e098b72e2d7ff1ebc3d6539af95
Yuanlong Tan <yt4xb@virginia.edu>
2019-02-22 14:07:51 -0500

    unify 'method'

--- OESS-Client/remove.py
+++ OESS-Client/remove.py
@@ -10,7 +10,7 @@ values1 = {}
 (username,passwd)=account.readAccount(sys.argv[2])
 wg_id=account.getWkGpID(sys.argv[1],username,passwd)
 ct_id=account.getCtID(wg_id, sys.argv[3],username,passwd)
-values1 = {'action' : 'remove_circuit', 'workgroup_id' : wg_id, 'circuit_id' : ct_id, 'remove_time' : -1}
+values1 = {'method' : 'remove_circuit', 'workgroup_id' : wg_id, 'circuit_id' : ct_id, 'remove_time' : -1}
 data = urllib.urlencode(values1, doseq=True)
 gh_url2 = 'https://al2s.net.internet2.edu/oess/services-kerb/provisioning.cgi'
 req = urllib2.Request(gh_url2, data)
@@ -28,5 +28,3 @@ if (searchResults == None):
 	call(["ulogger", "-i","-l","$LDM/var/logs/ldmd.log",jsonData['error_text']])
 else:
 	call(["ulogger", "-i","-l","$LDM/var/logs/ldmd.log","Remove.py: Success"])
-
-
