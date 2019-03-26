d316edcc0b4e353302af889251b8bb82541b07ef
Steven Emmerson <emmerson@ucar.edu>
2019-03-26 11:09:11 -0600

    Pulled oess

--- mcast_lib/OESS-Client/edit.py
+++ mcast_lib/OESS-Client/edit.py
@@ -16,7 +16,6 @@ import sys
 import time
 import string
 import json
-import account
 from subprocess import call
 
 def edit_endpoint(wg_id,node_id,interface_id,vlan_id,ct_id,function,username,passwd):
@@ -25,8 +24,6 @@ def edit_endpoint(wg_id,node_id,interface_id,vlan_id,ct_id,function,username,pas
 	node = []
 	interface = []
 	tag = []
-	link = []
-	link1 = []
 	gh_url = 'https://al2s.net.internet2.edu/oess/services-kerb/data.cgi'
 	values1 = {'method' : 'get_circuit_details', 'circuit_id' : ct_id}
 	data1 = urllib.urlencode(values1, doseq=True)
@@ -40,7 +37,6 @@ def edit_endpoint(wg_id,node_id,interface_id,vlan_id,ct_id,function,username,pas
 	result1 = handler1.read()
 	jsonData1 = json.loads(result1)
 	searchResults1 = jsonData1['results']['endpoints']
-	searchResults2 = jsonData1['results']['links']
 	for er in searchResults1:
 	    if er['node'] != None:
 		node.append(er['node'])
@@ -50,43 +46,22 @@ def edit_endpoint(wg_id,node_id,interface_id,vlan_id,ct_id,function,username,pas
 	for er in searchResults1:
 	    if er['tag'] != None:
 		tag.append(er['tag'])
-	for er in searchResults2:
-	    if er['name'] != None:
-		link.append(er['name'])
 	if function == 'add':
-		link1=account.getSPath(node[0],node_id,username,passwd)
-		for er in link1:
-			if er not in link:
-				link.append(er)
 		if node_id not in node:
 			node.append(node_id)
 		if interface_id not in interface:
 			interface.append(interface_id)
 		if vlan_id not in tag:
 			tag.append(vlan_id)
-	if function == 'del':
-		link1=account.getSPath(node[0], node_id,username,passwd)
-		for er in link1:
-			if er in link:
-				link.remove(er)
+	elif function == 'del':
 		if node_id in node:
 			node.remove(node_id)
 		if interface_id in interface:
 			interface.remove(interface_id)
 		if vlan_id in tag:
 			tag.remove(vlan_id)
-		index = 1
-		if len(node) == 1:
-			link = []
-		else:
-			while index < len(node):
-				link1=account.getSPath(node[0], node[index],username,passwd)
-				for er in link1:
-					if er not in link:
-						link.append(er['link'])
-				index += 1
-	values2 = {'method' : 'provision_circuit', 'workgroup_id' : wg_id, 'circuit_id' : ct_id, 'provision_time' : -1, 'remove_time' : -1, 'description' : sys.argv[3], 'link' : link, 'node' : node, 'interface' : interface, 'tag' : tag}
-	data = urllib.urlencode(values2, doseq=True)
+	values2 = {'method' : 'provision_circuit', 'workgroup_id' : wg_id, 'circuit_id' : ct_id, 'provision_time' : -1, 'remove_time' : -1, 'description' : sys.argv[3], 'node' : node, 'interface' : interface, 'tag' : tag}
+	data = urllib.urlencode(values2, doseq=True)	
 	gh_url2 = 'https://al2s.net.internet2.edu/oess/services-kerb/provisioning.cgi'
 	req = urllib2.Request(gh_url2, data)
 	password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
--- mcast_lib/OESS-Client/provision.py
+++ mcast_lib/OESS-Client/provision.py
@@ -22,11 +22,10 @@ global values1
 values1 = {}
 link = []
 (username,passwd)=account.readAccount(sys.argv[2])
-link=account.getSPath(sys.argv[4], sys.argv[7],username,passwd)
 wg_id=account.getWkGpID(sys.argv[1],username,passwd)
 ct_id=account.getCtID(wg_id, sys.argv[3],username,passwd)
 if ct_id == 0:
-	values1 = {'method' : 'provision_circuit', 'workgroup_id' : wg_id, 'provision_time' : -1, 'remove_time' : -1, 'description' : sys.argv[3], 'link' : link, 'node' : [sys.argv[4], sys.argv[7]], 'interface' : [sys.argv[5], sys.argv[8]], 'tag' : [sys.argv[6], sys.argv[9]]}
+	values1 = {'method' : 'provision_circuit', 'workgroup_id' : wg_id, 'provision_time' : -1, 'remove_time' : -1, 'description' : sys.argv[3], 'node' : [sys.argv[4], sys.argv[7]], 'interface' : [sys.argv[5], sys.argv[8]], 'tag' : [sys.argv[6], sys.argv[9]]}
 	data = urllib.urlencode(values1, doseq=True)
 	gh_url2 = 'https://al2s.net.internet2.edu/oess/services-kerb/provisioning.cgi'
 	req = urllib2.Request(gh_url2, data)
