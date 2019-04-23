8ba00a39aa8261a1c2f5404ca7115d3964ee4abe
Steven Emmerson <emmerson@ucar.edu>
2019-04-23 14:14:08 -0600

    Include "wmo_header.h" and cosmetics

--- noaaport/readpsh.c
+++ noaaport/readpsh.c
@@ -6,8 +6,8 @@
 #include <string.h>
 #include <log.h>
 #include "nport.h"
+#include "wmo_header.h"
 
-int wmo_header(char *prod_name, size_t prod_size, char *wmohead, char *wmometa, int *metaoff);
 void redbook_header(char *buf, int nbytes, char *tstr);
 
 int readpsh(char *buf,psh_struct *psh)
@@ -106,136 +106,156 @@ psh->origrunid = lval;
 return(iret);
 }
 
-int readccb(char *buf,ccb_struct *ccb, psh_struct *psh, int blen)
+int readccb(
+        char* const                  buf,
+        ccb_struct* const            ccb,
+        psh_struct* const            psh,
+        const int                    blen)
 {
-int iret=0;
-unsigned char b1,b2;
-char wmohead[256];
-int wmolen, metaoff=-1;
-char redbook_title[45];
-char wmometa[256];
+    int           iret = 0;
+    unsigned char b1;
+    unsigned char b2;
+    char          wmohead[256];
+    int           wmolen;
+    int           metaoff = -1;
+    char          redbook_title[45] = {};
+    char          wmometa[256] = {};
 
-redbook_title[0] = '\0';
-wmometa[0] = '\0';
+    b1 = (unsigned char)buf[0];
+    b2 = (unsigned char)buf[1];
 
-b1 = (unsigned char)buf[0];
-b2 = (unsigned char)buf[1];
+    ccb->b1 = b1;
+    ccb->len = 2 * (int)(((b1 & 63) << 8) + b2);
 
-ccb->b1 = b1;
-ccb->len = 2 * (int)(((b1 & 63) << 8) + b2);
+    if (ccb->len > blen) {
+        // A rouge product missing its CCB, reported to NWS DM 3/10/05 by Chiz
+        log_add("Invalid ccb length = %d %d %d, blen %d", ccb->len, b1, b2,
+                blen);
 
-if ( ccb->len > blen ) /* A rouge product missing its CCB, reported to NWS DM 3/10/05 by Chiz */
-   {
-   log_error_q ("invalid ccb length = %d %d %d, blen %d\n",ccb->len,b1,b2,blen);
+        /* try a failsafe header, otherwise use our own! */
+        wmolen = 0;
+        while (((int)buf[wmolen] >= 32) && (wmolen < 256))
+            wmolen++;
 
-   /* try a failsafe header, otherwise use our own! */
-   wmolen = 0;
-   while ( ( (int)buf[wmolen] >= 32) && ( wmolen < 256 ) ) wmolen++;
-   if ( wmolen > 0 )
-      strncat(psh->pname,buf,wmolen);
-   else
-      sprintf(psh->pname,"Unidentifiable product");
-   ccb->len = 0;
-   return(-1);
-   }
+        if (wmolen > 0) {
+            (void)strncat(psh->pname,buf,wmolen);
+        }
+        else {
+            (void)sprintf(psh->pname,"Unidentifiable product");
+        }
+
+        ccb->len = 0;
+        iret = -1;
+    }
+    else {
+        b1 = (unsigned char)buf[10];
+        b2 = (unsigned char)buf[11];
+        psh->ccbmode = b1;
+        psh->ccbsubmode = b2;
+        psh->hasccb = 1;
+
+        log_debug("ccb mode %d ccb submode %d", psh->ccbmode, psh->ccbsubmode);
+
+        b1 = (unsigned char)buf[12];
+        b2 = (unsigned char)buf[13];
+        ccb->user1 = b1;
+        ccb->user2 = b1;
+
+        log_debug("ccb user1 %d ccb user2 %d", ccb->user1, ccb->user2);
+
+        /* Initialize ccbdtype...eventually used to identify data type */
+        psh->ccbdtype[0] = '\0';
+
+        /* see if this looks like a WMO header, if so canonicalize */
+        if (wmo_header(buf+ccb->len, blen - ccb->len, wmohead, wmometa,
+                &metaoff) == 0) {
+            if (strlen(wmohead) > 0) {
+                strcat(psh->pname,wmohead);
+
+                if (metaoff > 0)
+                    psh->metaoff = metaoff;
+            }
+            else {
+                wmolen = 0;
+
+                while (((int)buf[wmolen + ccb->len] >= 32) && (wmolen < 256))
+                    wmolen++;
+
+                (void)strncat(psh->pname, buf+ccb->len, wmolen);
+            }
+        }
+        else {
+            wmolen = 0;
+
+            while (((int)buf[wmolen + ccb->len] >= 32) && (wmolen < 256))
+                wmolen++;
+
+            if (wmolen > 0) {
+                (void)strncat(psh->pname, buf+ccb->len, wmolen);
+            }
+            else {
+               (void)sprintf(psh->pname,"Unidentifiable product");
+            }
+
+            log_notice_q("Non-wmo product type %s ccbmode %d ccbsubmode %d\0",
+                 psh->pname, psh->ccbmode, psh->ccbsubmode);
+        }
+
+        /* for now, since NEXRAD is not identified as a separate PTYPE, use WMO header check */
+        if ((psh->ccbmode == 2)&&(psh->ccbsubmode == 0)) {
+            if ((memcmp(buf+ccb->len,"SDUS5",5) == 0)||
+                (memcmp(buf+ccb->len,"SDUS2",5) == 0)||
+                (memcmp(buf+ccb->len,"SDUS3",5) == 0)||
+                (memcmp(buf+ccb->len,"SDUS7",5) == 0)||
+                (memcmp(buf+ccb->len,"SDUS8",5) == 0)) {
+                   /* uncompressed nids check */
+                   if (ccb->user1 != 'F')
+                       (void)sprintf(psh->ccbdtype,"nids/");
+
+                   psh->pcat = PROD_CAT_NIDS;
+            }
+        }
+
+        /*
+        unfortunately, NWS isn't using CCB mode for all product types
+        else if((psh->pcat == PROD_CAT_GRID)&&(psh->ccbmode == 2)&&(psh->ccbsubmode == 1))
+           sprintf(psh->ccbdtype,"grib/\0");
+        else if(psh->pcat == PROD_CAT_POINT)
+           sprintf(psh->ccbdtype,"bufr/\0");
+        */
+        else if (psh->pcat == PROD_CAT_GRAPHIC) {
+           /* see if this is recognizeable as a redbook graphic */
+           redbook_header(buf+ccb->len,blen - ccb->len, redbook_title);
+
+           if (strlen(redbook_title) > 0) {
+               (void)sprintf(psh->ccbdtype, "redbook %d_%d/", ccb->user1,
+                       ccb->user2);
+           }
+           else {
+               (void)sprintf(psh->ccbdtype,"graph %d_%d/",  ccb->user1,
+                       ccb->user2);
+           }
+        }
+
+        /* create metadata */
+        (void)sprintf(psh->metadata, " !");
+
+        if (psh->ccbdtype[0] != '\0')
+            strcat(psh->metadata, psh->ccbdtype);
+
+        if (redbook_title[0] != '\0')
+            strcat(psh->metadata, redbook_title);
+
+        if (wmometa[0] != '\0')
+            strcat(psh->metadata, wmometa);
+
+        if (psh->metadata[2] == '\0')
+            psh->metadata[0] = '\0';
 
 
-b1 = (unsigned char)buf[10];
-b2 = (unsigned char)buf[11];
-psh->ccbmode = b1;
-psh->ccbsubmode = b2;
-log_debug("ccb mode %d ccb submode %d\0",psh->ccbmode,psh->ccbsubmode);
-psh->hasccb = 1;
+        /* restore meta data to PROD NAME
+        strcat(psh->pname,psh->metadata); */
+    } // `ccb` length isn't greater than allowed
 
-b1 = (unsigned char)buf[12];
-b2 = (unsigned char)buf[13];
-ccb->user1 = b1;
-ccb->user2 = b1;
-log_debug("ccb user1 %d ccb user2 %d\0",ccb->user1,ccb->user2);
-psh->ccbdtype[0] = '\0';     /* Initialize ccbdtype...eventually used to identify data type */
-
-/* see if this looks like a WMO header, if so canonicalize */
-if(wmo_header(buf+ccb->len,blen - ccb->len,wmohead,wmometa,&metaoff)==0)
-   {
-   if(strlen(wmohead) > 0)
-      {
-      strcat(psh->pname,wmohead); 
-      if (metaoff > 0) psh->metaoff = metaoff;
-      }
-   else
-      {
-      wmolen = 0;
-      while ( ( (int)buf[wmolen + ccb->len] >= 32) && ( wmolen < 256 ) )
-         wmolen++;
-
-      strncat(psh->pname,buf+ccb->len,wmolen);
-      }
-   }
-else
-   {
-   wmolen = 0;
-   while ( ( (int)buf[wmolen + ccb->len] >= 32) && ( wmolen < 256 ) )
-      wmolen++;
-   if ( wmolen > 0 )
-      strncat(psh->pname,buf+ccb->len,wmolen);
-   else
-      sprintf(psh->pname,"Unidentifiable product");
-
-   log_notice_q("Non-wmo product type %s ccbmode %d ccbsubmode %d\0",
-	psh->pname, psh->ccbmode, psh->ccbsubmode);
-   }
-
-
-/* for now, since NEXRAD is not identified as a separate PTYPE, use WMO header check */
-if((psh->ccbmode == 2)&&(psh->ccbsubmode == 0)) 
-   {
-   if((memcmp(buf+ccb->len,"SDUS5",5) == 0)||
-      (memcmp(buf+ccb->len,"SDUS2",5) == 0)||
-      (memcmp(buf+ccb->len,"SDUS3",5) == 0)||
-      (memcmp(buf+ccb->len,"SDUS7",5) == 0)||
-      (memcmp(buf+ccb->len,"SDUS8",5) == 0) )
-      {
-      /* uncompressed nids check */
-      if(ccb->user1 != 'F') sprintf(psh->ccbdtype,"nids/");
-      psh->pcat = PROD_CAT_NIDS;
-      }
-   }
-/*
-unfortunately, NWS isn't using CCB mode for all product types
-else if((psh->pcat == PROD_CAT_GRID)&&(psh->ccbmode == 2)&&(psh->ccbsubmode == 1))
-   sprintf(psh->ccbdtype,"grib/\0");
-else if(psh->pcat == PROD_CAT_POINT)
-   sprintf(psh->ccbdtype,"bufr/\0");
-*/
-else if (psh->pcat == PROD_CAT_GRAPHIC)
-   {
-   /* see if this is recognizeable as a redbook graphic */
-   redbook_header(buf+ccb->len,blen - ccb->len, redbook_title);
-   if(strlen(redbook_title) > 0)
-      sprintf(psh->ccbdtype,"redbook %d_%d/", ccb->user1, ccb->user2);
-   else
-      sprintf(psh->ccbdtype,"graph %d_%d/",  ccb->user1, ccb->user2);
-   }
-
-
-
-/* create metadata */
-sprintf(psh->metadata," !");
-
-if ( psh->ccbdtype[0] != '\0')
-   strcat ( psh->metadata, psh->ccbdtype);
-
-if ( redbook_title[0] != '\0' )
-   strcat( psh->metadata, redbook_title);
-
-if ( wmometa[0] != '\0' )
-   strcat( psh->metadata, wmometa);
-
-if ( psh->metadata[2] == '\0' ) psh->metadata[0] = '\0';
-
-
-/* restore meta data to PROD NAME
-strcat(psh->pname,psh->metadata); */
-
-return(iret);
+    return(iret);
 }
