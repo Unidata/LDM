fa9f0e85b662dcc99cf27daa3bc4822bbcbb15de
Steven Emmerson <emmerson@ucar.edu>
2019-06-22 14:10:48 -0600

    Changed implementation from Set<{sd,index}> to Map<sd,Set<index>}

--- mcast_lib/FMTP-LDM7/UnidataFMTP/FMTPv3/sender/SockToIndexMap.h
+++ mcast_lib/FMTP-LDM7/UnidataFMTP/FMTPv3/sender/SockToIndexMap.h
@@ -21,7 +21,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @brief     Map from socket descriptor to locked product indexes.
+ * @brief     Map from socket descriptor to unreleased product indexes.
  *
  * Allows data-products that a receiving node has locked to be released if the
  * connection to the node is broken.
@@ -30,6 +30,7 @@
 #ifndef MCAST_LIB_FMTP_LDM7_UNIDATAFMTP_FMTPV3_SENDER_SOCKTOINDEXMAP_H_
 #define MCAST_LIB_FMTP_LDM7_UNIDATAFMTP_FMTPV3_SENDER_SOCKTOINDEXMAP_H_
 
+#include <map>
 #include <memory>
 #include <mutex>
 #include <utility>
@@ -37,31 +38,19 @@
 
 class SockToIndexMap final
 {
-    typedef std::pair<int,uint32_t> Element;
+    typedef std::set<uint32_t>     Set;
+    typedef std::map<int, Set>     Map;
+    typedef std::mutex             Mutex;
+    typedef std::lock_guard<Mutex> Guard;
 
-    static bool compare(
-            const Element& lhs,
-            const Element& rhs)
-    {
-        return (lhs.first < rhs.first)
-                ? true
-                : (lhs.first > rhs.first)
-                  ? false
-                  : (lhs.second < rhs.second);
-    }
-
-    typedef std::set<Element, decltype(&compare)> Set;
-    typedef std::mutex                            Mutex;
-    typedef std::lock_guard<Mutex>                Guard;
-
-    Set           set;
+    Map           map;
     mutable Mutex mutex;
 
 public:
-    typedef Set::iterator                         iterator;
+    typedef Map::iterator iterator;
 
     SockToIndexMap()
-        : set(compare)
+        : map()
         , mutex()
     {}
 
@@ -72,7 +61,7 @@ public:
         Guard guard(mutex);
 
         for (auto sd : socks)
-            set.emplace(sd, index);
+            map[sd].insert(index);
     }
 
     void erase(
@@ -81,7 +70,10 @@ public:
     {
         Guard guard(mutex);
 
-        set.erase(Element(sock, index));
+        auto indexes = map.find(sock);
+
+        if (indexes != map.end())
+            indexes->second.erase(index);
     }
 
     void erase(
@@ -90,28 +82,31 @@ public:
     {
         Guard guard(mutex);
 
-        for (auto sd : socks)
-            set.erase(Element(sd, index));
+        for (auto sd : socks) {
+            auto indexes = map.find(sd);
+
+            if (indexes != map.end())
+                indexes->second.erase(index);
+        }
     }
 
     void erase(const int sd)
     {
         Guard guard(mutex);
-        set.erase(set.lower_bound({sd,0}), set.upper_bound({sd,UINT32_MAX}));
+        map.erase(sd);
     }
 
-    typedef std::shared_ptr<std::vector<uint32_t>> FindResult;
+    typedef std::shared_ptr<Set> FindResult;
 
     FindResult find(const int sd) const
     {
         Guard guard(mutex);
-        auto* indexes = new FindResult::element_type(set.size());
+        auto  indexes = map.find(sd);
 
-        for (auto elt = set.lower_bound({sd,0}),
-                end = set.upper_bound({sd,UINT32_MAX}); elt != end; ++elt)
-            indexes->push_back(elt->second);
-
-        return FindResult(indexes);
+        return FindResult(
+                (indexes == map.end())
+                    ? new Set()
+                    : new Set(indexes->second));
     }
 };
 
