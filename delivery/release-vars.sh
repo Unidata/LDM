3118fa5022682007fb6736a5eb5048fb9c6477c8
Steven Emmerson <emmerson@ucar.edu>
2020-05-01 14:52:14 -0600

    v6.13.12.49

--- delivery/release-vars.sh
+++ delivery/release-vars.sh
@@ -3,7 +3,7 @@
 # Base definitions:
 export USER_NAME=ldm
 export PKG_NAME=ldm
-export PKG_VERSION=6.13.12.48
+export PKG_VERSION=6.13.12.49
 export SOURCE_DISTRO_EXT=tar.gz
 export ACCEPTANCE_CONFIGURE_OPTS="--disable-root-actions --with-noaaport"
 export ABSPATH_FTP_PUB_DIR=/web/ftp/pub
