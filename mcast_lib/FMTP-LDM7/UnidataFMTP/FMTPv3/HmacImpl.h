0418e390c59cac1bf9f78494760bfacc6fa592dc
Steven Emmerson <emmerson@ucar.edu>
2021-02-23 16:03:15 -0700

    Correct indentation

--- mcast_lib/FMTP-LDM7/UnidataFMTP/FMTPv3/HmacImpl.h
+++ mcast_lib/FMTP-LDM7/UnidataFMTP/FMTPv3/HmacImpl.h
@@ -18,79 +18,79 @@
 
 class HmacImpl
 {
-	std::string key;   ///< HMAC key
-	EVP_PKEY*   pkey;  ///< OpenSSL HMAC key
-	EVP_MD_CTX* mdCtx; ///< Message-digest context
+    std::string key;   ///< HMAC key
+    EVP_PKEY*   pkey;  ///< OpenSSL HMAC key
+    EVP_MD_CTX* mdCtx; ///< Message-digest context
 
-	void init(const std::string& key);
+    void init(const std::string& key);
 
 public:
-	/**
-	 * Default constructs. A new HMAC key will be pseudo-randomly chosen.
-	 *
+    /**
+     * Default constructs. A new HMAC key will be pseudo-randomly chosen.
+     *
      * @throw std::runtime_error  OpenSSL failure
-	 */
-	HmacImpl();
+     */
+    HmacImpl();
 
-	/**
-	 * Constructs from an HMAC key.
-	 *
-	 * @param[in] key                Key for computing HMAC-s. The length of the
-	 *                               key must be at least `2*MAC_SIZE`.
+    /**
+     * Constructs from an HMAC key.
+     *
+     * @param[in] key                Key for computing HMAC-s. The length of the
+     *                               key must be at least `2*MAC_SIZE`.
      * @throw std::runtime_error     OpenSSL failure
      * @throw std::invalid_argument  `key.size() < 2*MAC_SIZE`
-	 */
-	HmacImpl(const std::string& key);
+     */
+    HmacImpl(const std::string& key);
 
-	HmacImpl(const HmacImpl& hmac) =delete;
+    HmacImpl(const HmacImpl& hmac) =delete;
 
-	/**
-	 * Destroys.
-	 */
-	~HmacImpl();
+    /**
+     * Destroys.
+     */
+    ~HmacImpl();
 
-	HmacImpl& operator=(const HmacImpl& rhs) =delete;
+    HmacImpl& operator=(const HmacImpl& rhs) =delete;
 
-	HmacImpl& operator=(HmacImpl&& rhs);
+    HmacImpl& operator=(HmacImpl&& rhs);
 
-	/**
-	 * Returns the key for computing HMAC-s.
-	 *
-	 * @return  The HMAC key
-	 */
-	const std::string& getKey() const noexcept {
-		return key;
-	}
+    /**
+     * Returns the key for computing HMAC-s.
+     *
+     * @return  The HMAC key
+     */
+    const std::string& getKey() const noexcept {
+        return key;
+    }
 
-	/**
-	 * Returns the message authentication code (MAC) of an FMTP message.
-	 *
-	 * @param[in]  header         FMTP header
-	 * @param[in]  payload        FMTP message payload. `header.payloadlen` is
-	 *                            the length in bytes. Ignored if zero length.
-	 * @param[out] mac            Computed MAC
-	 * @throw std::logic_error    `header.payloadlen > 0 && `payload == nullptr`
-	 * @throw std::runtime_error  OpenSSL failure
-	 */
-	void getMac(const FmtpHeader& header,
-			    const void*       payload,
-				char              mac[MAC_SIZE]);
+    /**
+     * Returns the message authentication code (MAC) of an FMTP message.
+     *
+     * @param[in]  header         FMTP header
+     * @param[in]  payload        FMTP message payload. `header.payloadlen` is
+     *                            the length in bytes. Ignored if zero length.
+     * @param[out] mac            Computed MAC
+     * @throw std::logic_error    `header.payloadlen > 0 && `payload == nullptr`
+     * @throw std::runtime_error  OpenSSL failure
+     */
+    void getMac(const FmtpHeader& header,
+                const void*       payload,
+                char              mac[MAC_SIZE]);
 
-	/**
-	 * Returns the string representation of a MAC.
-	 * @param[in] mac  MAC
-	 * @return         String representation
-	 */
-	static std::string to_string(const char mac[MAC_SIZE]);
+    /**
+     * Returns the string representation of a MAC.
+     * @param[in] mac  MAC
+     * @return         String representation
+     */
+    static std::string to_string(const char mac[MAC_SIZE]);
 
-	/**
-	 * Indicates if HMAC usage is disabled. Usage is disabled if the environment
-	 * variable `DISABLE_HMAC` is defined and not the empty string,
-	 *
-	 * @retval `true`   HMAC usage is disabled
-	 * @retval `false`  HMAC usage is not disabled
-	 */
-	static bool isDisabled();
+    /**
+     * Indicates if HMAC usage is disabled. Usage is disabled if the environment
+     * variable `DISABLE_HMAC` is defined and not the empty string,
+     *
+     * @retval `true`   HMAC usage is disabled
+     * @retval `false`  HMAC usage is not disabled
+     */
+    static bool isDisabled();
 };
 
 #endif /* MCAST_LIB_FMTP_LDM7_UNIDATAFMTP_FMTPV3_HMACIMPL_H_ */
