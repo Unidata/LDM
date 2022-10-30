eff6aa3b00e6702c752b24a805492356023a9bf9
Steven Emmerson <emmerson@ucar.edu>
2022-10-30 11:09:24 -0600

    Cosmetics

--- log/log.h
+++ log/log.h
@@ -379,8 +379,7 @@ bool log_is_level_enabled(
 /**
  * Logs a single message at the DEBUG level, bypassing the message-queue.
  *
- * @param[in] ...  Optional arguments of the message -- starting with the format
- *                 of the message.
+ * @param[in] ...      Optional arguments of the message -- starting with the format of the message.
  * @asyncsignalsafety  Unsafe
  */
 #define log_debug(...) do {\
