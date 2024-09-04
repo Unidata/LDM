83014ab3acb36cbf31fb465c351d61ab996f1b52
Stonie Cooper <cooper@ucar.edu>
2024-09-04 15:18:48 +0000

    Updated all files for absence of blender(1)

--- noaaport/killNoaaport.sh
+++ noaaport/killNoaaport.sh
@@ -3,7 +3,3 @@ for procNum in `ps -ef | grep noaaportIngester | grep -v grep | awk '{print $2}'
 	echo $procNum
 	kill -9 $procNum
 done
-for procNum in `ps -ef | grep blender | grep -v grep | awk '{print $2}'`; do
-	echo $procNum
-	kill -9 $procNum
-done
